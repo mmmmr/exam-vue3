aad6230dff22187455847e9ba0e871a75bff8cb7
xuheng <ob12564@163.com>
2022-11-20 20:59:49 +0800

    feat(init): init
    router

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <router-link to="/login"> toLogin </router-link>
+  <router-link to="/main" >  toMain </router-link>
+  <router-view/>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+</style>
